748963891b754ba8b6708ed1954089ebb28a0038
Keith Hekker <khekker@gmail.com>
2013-09-03 17:17:24 -0400

    added has_ShutdownbuttonBeenPressed

--- obd_capture.py
+++ obd_capture.py
@@ -4,21 +4,24 @@ import obd_io
 import serial
 import platform
 import obd_sensors
-#from datetime import datetime
 from PIL import Image,ImageDraw,ImageFont
 import ImageOps
 import nokiaSPI
 import time
 import os
 import sqlite3
-#from datetime import timedelta
-
+import wiringpi
 from obd_utils import scanSerial
+INPUT=0
+OUTPUT=1
 
 class OBD_Capture():
 	def __init__(self):
 		self.port = None
-		#localtime = time.localtime(time.time())
+		wiringpi.wiringPiSetup()
+		wiringpi.pinMode(5,INPUT)  #Physical pin 18, BCM GPIO 24
+		wiringpi.pinMode(6,OUTPUT)	#Physical pin 22, BCM GPIO 25
+		wiringpi.digitalWrite(6,1)  #Set output high 
 
 	def connect(self):
 		portnames = scanSerial()
@@ -43,39 +46,26 @@ class OBD_Capture():
 		noki.cls()
 		im = Image.new('1', (84,48))
 		draw = ImageDraw.Draw(im)
-		print cShortTerm
 		draw.text((0,0),cShortTerm, font=font, fill=1)
 		draw.text((0,24),cLongTerm, font=font, fill=1)
 		draw.text((54,7),"3 s", font=fontsmall, fill=1)
 		draw.text((54,31),cLongTermMinutes + " m", font=fontsmall, fill=1)
 		# Copy it to the display
 		noki.show_image(im)
-		#noki.next_row()
 		
 	def ComputeFuelConsumption(self):
 		nStartSec = time.time() - 20
 		
 		try:
 			cLimit = " and time_read > " + str(nStartSec) + " order by time_read desc limit 6)"
-			#cursor.execute('''SELECT maf,speed from SensorReadings where speed > "0" and maf > "0" and rpm != "NODATA" ''' + cLimit)
-			#data = cursor.fetchall()
 			self.cursor.execute('''SELECT  avg((3600 * maf)/(9069.90 * speed)) FROM (select maf,speed from SensorReadings where speed > "0" and maf > "0" and rpm != "NODATA" ''' + cLimit)
 			data = self.cursor.fetchone()
 		except sqlite3.OperationalError,msg:
 			return msg
-		#print len(data)
+		
 		if (len(data) > 0):
-			#nFuelConsumption = 0
-			#for x in range(0,len(data)):
-			#	nFuelConsumption += (3600 * float(data[x][0]))/(9069.90 * float(data[x][1]))
-				
-			#nAvgFuelConsumption = nFuelConsumption/len(data)
 			nAvgFuelConsumption = data[0]
-			#print nAvgFuelConsumption
-			#print type(nAvgFuelConsumption)
-			#print data[x][0],data[x][1],data[x][2]
-			
-			print type(nAvgFuelConsumption)
+						
 			try:
 				nAvgFuelConsumption = float(nAvgFuelConsumption)
 				return "{:5.2f}".format(nAvgFuelConsumption).lstrip()
@@ -90,21 +80,23 @@ class OBD_Capture():
 			float(DataToTest)
 			return True
 		except ValueError:
-			return False			
+			return False
 
-				
+	def has_ShutdownButtonBeenPressed(self):
+		if (wiringpi.digitalRead(5) == 1):
+			self.cursor.close()
+			self.conn.close()
+			time.sleep(0.5)
+			os.system("sudo poweroff")		
+		
 	def capture_data(self):
 		#Creating new database
 		for kounter in range(10000):
 			cKounter = "{0:05d}".format(kounter)
 			cNewDatabase = "obdii" + cKounter + ".db"
-			#print cNewDatabase
 			if not (os.path.exists(cNewDatabase)):
-				#print "New database name: " + cNewDatabase
 				break
 
-		#global conn
-		#global cursor
 		self.conn = sqlite3.connect(cNewDatabase)
 		self.cursor = self.conn.cursor()
 			
@@ -128,12 +120,11 @@ class OBD_Capture():
 		sqlInsertTemplate = "INSERT INTO SensorReadings(time_read, "
 		
 		for supportedSensor in self.supportedSensorList:
-			#print "supported sensor index = " + str(supportedSensor[0]) + " " + str(supportedSensor[1].shortname)
 			sqlCreateTable += str(supportedSensor[1].shortname)  + " text,"	
 			sqlInsertTemplate += str(supportedSensor[1].shortname)  + ","
 			
 		sqlCreateTable = sqlCreateTable[:sqlCreateTable.rfind(",")] + ")"
-		#print sqlCreateTable
+		
 		try:
 			self.cursor.execute(sqlCreateTable)
 			self.conn.commit()
@@ -146,7 +137,6 @@ class OBD_Capture():
 		noki.text(cMessage,wrap=True)
 		
 		sqlInsertTemplate = sqlInsertTemplate[:sqlInsertTemplate.rfind(",")] + ") VALUES ("
-		#print sqlInsertTemplate
 		
 		time.sleep(3)
 
@@ -159,22 +149,17 @@ class OBD_Capture():
 			nStartTime = time.time()
 			x = 0
 			while True:
+				has_ShutdownButtonBeenPressed(self)
 				current_time = time.time()
-				#current_time = str(localtime.hour)+":"+str(localtime.minute)+":"+str(localtime.second)+"."+str(localtime.microsecond)
-				#log_string = current_time + "\n"
 				sqlInsert = sqlInsertTemplate + '"' + str(current_time) + '",'
 				results = {}
 				for supportedSensor in self.supportedSensorList:
 					sensorIndex = supportedSensor[0]
-					#print sensorIndex
 					(name, value, unit) = self.port.sensor(sensorIndex)
-					#log_string += name + " = " + str(value) + " " + str(unit) + "\n"
-					#print value,type(value)
 					sqlInsert += '"' + str(value) + '",'	
 				
 				
 				sqlInsert = sqlInsert[:sqlInsert.rfind(",")] + ")"
-				#print sqlInsert
 
 				try:
 					self.cursor.execute(sqlInsert)
@@ -197,9 +182,6 @@ class OBD_Capture():
 				
 				cDurationInMinutes = "{:3.0f}".format((current_time - nStartTime)/60).lstrip()
 				self.nokiprint(cFuelConsumption,cTripAverage,cDurationInMinutes)
-					
-				#print log_string,
-				#time.sleep(0.5)
 				
 
 		except KeyboardInterrupt:
